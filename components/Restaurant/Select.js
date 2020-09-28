f77b078860852d3f964425b6a3b1f3f0e6130f50
nohgijin <ynda11111@naver.com>
2020-09-28 16:34:02 +0900

    [#10] markup: 기숙사 선택 selectbox 생성

---
+++ components/Restaurant/Select.js
@@ -0,0 +1,16 @@
+import '../../styles/Restaurant/select.scss'
+
+const Select = () => {
+  return (
+    <div className="select">
+      <div className="select-box">본관</div>
+      <div className="select-box">양성재</div>
+      <div className="select-box">양성재</div>
+      <div className="select-box">한빛식당</div>
+      <div className="select-box">별빛식당</div>
+      <div className="select-box">은하수식당</div>
+    </div>
+  )
+}
+
+export default Select
--- pages/restaurant.js
+++ pages/restaurant.js
@@ -3,12 +3,14 @@ import '../styles/Restaurant/restaurant.scss'
 
 import Title from '../components/Restaurant/Title'
 import Date from '../components/Restaurant/Date'
+import Select from '../components/Restaurant/Select'
 
 const Restaurant = () => {
   return (
     <div className="restaurant">
       <Title className="title" />
       <Date className="date" />
+      <Select className="select" />
     </div>
   )
 }
