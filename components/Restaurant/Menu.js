8b4243636b5fc87fa9415f43f12b451326194229
nohgijin <ynda11111@naver.com>
2020-09-30 22:10:55 +0900

    [#19] feature: 식단 더미 데이터 생성 및 view 부분 처리

--- components/Restaurant/Menu.js
+++ components/Restaurant/Menu.js
@@ -1,35 +1,65 @@
 import '../../styles/Restaurant/menu.scss'
 
 const Menu = () => {
+  const testData = [
+    {
+      restaurant_name: '본관',
+      food_name: `잡곡밥(쌀:국내산)
+      사골떡국
+      (우사골:호주산) 치킨너겟*머스타드
+      (계육:국내산)
+      오이지무침
+      김치 (배추/고춧가루:중국산)
+      우유 또는두유
+      736kcal/28g
+      `,
+      date: '2020-09-27',
+      day: '일',
+      time: 1,
+    },
+    {
+      restaurant_name: '본관',
+      food_name: `잡곡밥 (쌀:국내산)
+      시래기국
+      제육고추장불고기
+      (돈육:국내산)
+      숙주맛살무침
+      깍두기
+      821kcal/29g
+      `,
+      date: '2020-09-27',
+      day: '일',
+      time: 2,
+    },
+    {
+      restaurant_name: '본관',
+      food_name: `잡곡밥(쌀:국내산)
+      만두계란국
+      오징어야채볶음*소면사리
+      (오징어:페루산)
+      콩나물무침
+      김치 (배추/고춧가루:중국산)
+      석류차
+      857kcal/28g
+      `,
+      date: '2020-09-27',
+      day: '일',
+      time: 3,
+    },
+  ]
+  const timeList = ['아침', '점심', '저녁']
   return (
     <div className="restaurant-menu">
-      <div className="restaurant-time">
-        아침
-        <div className="restaurant-text">
-          <div className="content">흰밥/우유(두유)/김치</div>
-          <div className="content">북어미역국</div>
-          <div className="content">마파두부</div>
-          <div className="content">볼비엔나감자조림</div>
-        </div>
-      </div>
-      <div className="restaurant-time">
-        점심
-        <div className="restaurant-text">
-          <div className="content">흰밥/우유(두유)/김치</div>
-          <div className="content">북어미역국</div>
-          <div className="content">마파두부</div>
-          <div className="content">볼비엔나감자조림</div>
-        </div>
-      </div>
-      <div className="restaurant-time">
-        저녁
-        <div className="restaurant-text">
-          <div className="content">흰밥/우유(두유)/김치</div>
-          <div className="content">북어미역국</div>
-          <div className="content">마파두부</div>
-          <div className="content">볼비엔나감자조림</div>
-        </div>
-      </div>
+      {testData.map((d) => {
+        return (
+          <div className="restaurant-time">
+            {timeList[d.time - 1]}
+            <div className="restaurant-text">
+              <div className="content">{d.food_name}</div>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
