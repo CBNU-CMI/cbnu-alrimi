232db448171590f3ac13c7160736e0db1dd1177b
nohgijin <ynda11111@naver.com>
2020-10-06 17:33:49 +0900

    [#21] markup: 타임라인 마크업 작업 캘린더 페이지에 들어가는 타임라인 라벨 작업 완료

--- components/Schedule/Calendar.js
+++ components/Schedule/Calendar.js
@@ -1,7 +1,7 @@
 import '../../styles/Schedule/calendar.scss'
 
 const Calendar = () => {
-  return <div className="calendar" />
+  return <div className="calendar">일일일</div>
 }
 
 export default Calendar
--- pages/schedule.js
+++ pages/schedule.js
@@ -2,12 +2,14 @@ import mainLayout from '../layout/main'
 
 import Date from '../components/Schedule/Date'
 import Calendar from '../components/Schedule/Calendar'
+import Academic from '../components/Schedule/Academic'
 
 const Schedule = () => {
   return (
     <div className="schedule">
       <Date className="date" />
       <Calendar className="calendar" />
+      <Academic className="academic" />
     </div>
   )
 }
