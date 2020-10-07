7e3ffe8e175d4471db506f0fd8e2c654c49f9462
nohgijin <ynda11111@naver.com>
2020-10-07 22:20:56 +0900

    [#35] feature: 타임라인 시간 데이터 형식 변경

--- components/Schedule/Label.js
+++ components/Schedule/Label.js
@@ -1,15 +1,20 @@
 import '../../styles/Schedule/label.scss'
-import dateFormat from '../../common/dateForamt'
+import { dayList } from '../constants'
 
 const Label = ({ startDate, endDate, content }) => {
+  const sDate = new Date(startDate)
+  const eDate = new Date(endDate)
   return (
     <div className="label">
       <div className="content">{content}</div>
       {!endDate ? (
-        <div className="term">{dateFormat(startDate)}</div>
+        <div className="term">
+          {sDate.getMonth() + 1}.{sDate.getDate()}({dayList[sDate.getDay()]})
+        </div>
       ) : (
         <div className="term">
-          {dateFormat(startDate)}~{dateFormat(endDate)}
+          {sDate.getMonth() + 1}.{sDate.getDate()}({dayList[sDate.getDay()]})~
+          {eDate.getMonth() + 1}.{eDate.getDate()}({dayList[sDate.getDay()]})
         </div>
       )}
     </div>
