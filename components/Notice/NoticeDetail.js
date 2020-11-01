2bb12340152c9a6ca2faffa90f9c46b97a5b77fb
제리 <jaryapp@gmail.com>
2020-11-01 17:31:21 +0900

    [#47] feat: server api 분리

--- components/Notice/NoticeDetail.js
+++ components/Notice/NoticeDetail.js
@@ -1,19 +1,15 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import dateFormat from '../../common/dateFormat'
-import '../../styles/Notice/notice.scss'
+import { useEffect, useState } from 'react';
+import dateFormat from '../../common/dateFormat';
+import '../../styles/Notice/notice.scss';
+import { getNotice } from '../../api';
 
 const NoticeDetail = ({ noticeId }) => {
-  const [notice, setNotice] = useState({})
+  const [notice, setNotice] = useState({});
   useEffect(() => {
-    axios
-      .get(`http://${window.location.hostname}:3000/notice`, {
-        params: { noticeId },
-      })
-      .then((res) => {
-        setNotice(res.data)
-      })
-  }, [])
+    getNotice({ noticeId }).then((res) => {
+      setNotice(res.data);
+    });
+  }, []);
   return (
     <div className="notice-detail">
       <div className="title">
@@ -28,7 +24,7 @@ const NoticeDetail = ({ noticeId }) => {
       />
       <div>자세한 내용 및 첨부파일은 본문확인</div>
     </div>
-  )
-}
+  );
+};
 
-export default NoticeDetail
+export default NoticeDetail;
