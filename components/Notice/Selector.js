59e4109c8269ef633b3a9a55825ea8b6b85c9e66
제리 <jaryapp@gmail.com>
2020-11-09 17:04:50 +0900

    [#53] update: main selector

--- components/Notice/Selector.js
+++ components/Notice/Selector.js
@@ -1,39 +1,74 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React, { useCallback } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { selectTypeAction } from '../../reducers/noticeSelect'
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectTypeAction } from '../../reducers/noticeSelect';
+import { ConfigDialog } from '../Dialog';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 const Selector = () => {
-  const dispatch = useDispatch()
-  const selected = useSelector((state) => state.noticeSelect)
+  const dispatch = useDispatch();
+  const selected = useSelector((state) => state.noticeSelect);
+  const [scrolling, setScrolling] = useState(false);
 
   const onClickSelect = useCallback((select) => {
-    dispatch(selectTypeAction(select))
-  }, [])
+    dispatch(selectTypeAction(select));
+  }, []);
+
+  const configDialogRef = useRef();
+  function openConfigDialog() {
+    configDialogRef.current.openDialog();
+  }
+  function scroll() {
+    if (window.scrollY == 0) {
+      setScrolling(false);
+      return;
+    }
+    setScrolling(true);
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', scroll);
+  });
+
+  const data = [
+    {
+      id: 130101,
+      name: '소프트웨어학과',
+    },
+    {
+      id: 130201,
+      name: '전기공학부',
+    },
+    {
+      id: 130301,
+      name: '전자공학부',
+    },
+    {
+      id: 130401,
+      name: '정보통신공학부',
+    },
+  ];
 
   return (
-    <div className="notice-selector">
-      <div
-        onClick={() => onClickSelect('전공')}
-        className={selected === '전공' ? 'selected' : ''}
-      >
-        전공
+    <>
+      <div className={'notice-selector ' + (scrolling ? 'border' : '')}>
+        {data.map((d) => (
+          <div
+            className={selected == d.id ? 'selected' : ''}
+            key={d.id}
+            onClick={() => onClickSelect(d.id)}
+          >
+            {d.name}
+          </div>
+        ))}
+        <div className="config" onClick={openConfigDialog}>
+          <AiOutlinePlus />
+        </div>
       </div>
-      <div
-        onClick={() => onClickSelect('공통')}
-        className={selected === '공통' ? 'selected' : ''}
-      >
-        공통
-      </div>
-      <div
-        onClick={() => onClickSelect('스크랩')}
-        className={selected === '스크랩' ? 'selected' : ''}
-      >
-        스크랩
-      </div>
-    </div>
-  )
-}
+      <ConfigDialog ref={configDialogRef} />
+    </>
+  );
+};
 
-export default Selector
+export default Selector;
--- reducers/noticeSelect.js
+++ reducers/noticeSelect.js
@@ -1,20 +1,20 @@
-export const initialState = '전공'
+export const initialState = 130101;
 
-export const SELECT_TYPE = 'SELECT_TYPE'
+export const SELECT_TYPE = 'SELECT_TYPE';
 
 export const selectTypeAction = (selected) => ({
   type: SELECT_TYPE,
   selected,
-})
+});
 
 const reducer = (state = initialState, action) => {
   // 리듀서
   switch (action.type) {
     case SELECT_TYPE:
-      return action.selected
+      return action.selected;
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default reducer
+export default reducer;
