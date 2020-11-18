1d1cd21667595c2ff77997717fabd4a29b9dccc9
제리 <jaryapp@gmail.com>
2020-11-18 23:00:17 +0900

    [#85] api: token management

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import mainLayout from '../layout/main';
+import { inToken, setToken } from '../common/token';
 import { NoticeSelector, NoticeListBySiteId } from '../components/Notice';
 
 const Index = () => {
+  const router = useRouter();
+
   const selected = useSelector((state) => {
     return state.noticeSelect;
   });
+
+  useEffect(() => {
+    if (!inToken()) {
+      if (router.query.token) {
+        const token = router.query.token;
+        setToken(token);
+        location.reload();
+      }
+    }
+  }, [router.query.token]);
 
   return (
     <div className="notice-page">
